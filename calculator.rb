dc24174fc50d8fb0b9d5fff96403f0006ce10a7b
Aman Garg <a.garg8839@gmail.com>
2024-05-30 16:38:45 +0530

    rspec: raises ArgumentError for invalid input with a comma followed by a newline

--- calculator.rb
+++ calculator.rb
@@ -6,12 +6,17 @@ module StringCalculator
       return 0 if string.empty?
 
       numbers = split_string(string)
+      validate_array(numbers)
       numbers.map(&:to_i).sum
     end
 
     def split_string(string)
       numbers = string.delete(' ')
-      numbers&.split(/|,|\n/)
+      numbers&.split(/#{Regexp.escape(',')}|,|\n/)
+    end
+
+    def validate_array(numbers)
+      raise ArgumentError, 'Invalid Input' if numbers&.any?(&:empty?)
     end
   end
 end
