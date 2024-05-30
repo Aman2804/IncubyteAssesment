0246437e9c0154d385ed89acdabed69b349d68be
Aman Garg <a.garg8839@gmail.com>
2024-05-30 15:58:13 +0530

    Add rspec for input must string

---
+++ spec/string_calc_spec.rb
@@ -0,0 +1,13 @@
+
+RSpec.describe StringCalc do
+
+  # Add instance of tested class
+  subject(:input_value) { described_class.new }
+
+  describe "Add Function" do
+     
+    it 'if input is not a string' do
+      expect { input_value.add(10) }.to raise_error(ArgumentError, 'Input must be a string')
+    end 
+  end
+end
